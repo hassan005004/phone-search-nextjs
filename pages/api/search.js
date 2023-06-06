ee4f42e4df46b36444a2f2f06dc000c092b5c21e
Hassan Mehmood <hassan00942g@gmail.com>
2023-06-06 21:53:47 +0500

    asd

--- pages/api/search.js
+++ pages/api/search.js
@@ -5,7 +5,10 @@ import {
     removeDuplicates,
 } from '../../utils/scraping.js';
 
+
 export default async function handler(req, res) {
+    
+
     const start = performance.now();
 
     const query = req.query.query;
@@ -17,7 +20,31 @@ export default async function handler(req, res) {
         return res.status(400).json({ error: 'Missing query parameter' });
     }
 
-    const arrayOfPromptResults = await getURLs(query, number);
+    // const arrayOfPromptResults = await getURLs(query, number);
+    const arrayOfPromptResults = [
+        {
+        urls: [
+        'https://urbanicebotanicals.com/product/feel-free-wellness-tonic/',
+        'http://haleakava.com/index.html',
+        'https://www.mazatecgarden.com/products/kavakava.htm',
+        'https://www.realkava.com/',
+        'https://kavamamastore.com/collections/frontpage',
+        ],
+        metaphorSearchPrompt:
+        'I found a local store that sells kava root drinks like feel frees here:',
+        },
+        {
+        urls: [
+        'https://www.etsy.com/shop/ILOVEfelt88',
+        'https://thefeltstore.com/',
+        'https://www.etsy.com/shop/WoolFeltDepot?ref=simple-shop-header-name&listing_id=549379642',
+        'https://feelgoodfibers.com/',
+        'https://www.etsy.com/shop/feltcafe',
+        ],
+        metaphorSearchPrompt:
+        'Another option for purchasing feel frees locally can be found at this store:',
+        },
+        ];
     const dataPromises = arrayOfPromptResults
         .filter((result) => result !== undefined)
         .map(async ({ urls, metaphorSearchPrompt }) => {
